e80f6213acb5190dbaa2bffadeeb08bb7572296d
cyvision13 <cyvision13@outlook.com>
2020-11-10 03:35:18 -0500

    Setting up Express and Basic Routing

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+
 const express = require('express');
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Hello from the server side!');
+})
+
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
