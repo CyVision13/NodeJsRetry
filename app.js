8a9f9ac00f1d11bb918dff55bd328062c24ff022
cyvision13 <cyvision13@outlook.com>
2020-11-10 09:57:48 -0500

    Handling POST Requests

--- app.js
+++ app.js
@@ -33,9 +33,22 @@ app.get('/api/v1/tours', (req, res) => {
 
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
-  const newId = tours[tours.length -1].id +1;
-  const newTour = Object.assign({ id: newId},req.body)
-  res.send('Done');
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 const port = 3000;
 
