b760a856181b594f2923b297cebf005c4f606791
cyvision13 <cyvision13@outlook.com>
2020-11-10 08:08:12 -0500

    Starting Our API Handling GET Requests

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello from the server side!',
-    app: 'Natours'
-  });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello from the server side!',
+//     app: 'Natours'
+//   });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length ,
+    data: {
+      tours
+    }
+  });
 });
 
 const port = 3000;
