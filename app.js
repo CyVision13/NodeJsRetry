b6c86f9700eda50218f4b7b7647b7b7edd3ebb67
cyvision13 <cyvision13@outlook.com>
2020-11-11 12:33:38 -0500

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -73,6 +73,26 @@ app.post('/api/v1/tours', (req, res) => {
     }
   );
 });
+
+
+app.patch('/api/v1/tours/:id',(req,res)=>{
+  const id = req.params.id *1
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status:'success',
+    data:{
+      tour:'<Updated tour here...>>'
+    }
+  })
+})
+
 const port = 3000;
 
 app.listen(port, () => {
