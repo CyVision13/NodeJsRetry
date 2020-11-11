5f5e0958623badb1dc6605e6b77ef3ffd4d08390
cyvision13 <cyvision13@outlook.com>
2020-11-11 09:11:16 -0500

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -31,6 +31,22 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+
+
+app.get('/api/v1/tours/:id/:x?', (req, res) => {
+  console.log(req.params);
+
+  
+  res.status(200).json({
+    // status: 'success',
+    // results: tours.length,
+    // data: {
+    //   tours
+    // }
+  });
+});
+
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
