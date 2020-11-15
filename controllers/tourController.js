a88e9055ae2298740058347db2ea2b8868825978
cyvision13 <cyvision13@outlook.com>
2020-11-15 01:31:46 -0500

    Reading Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,48 +1,57 @@
 const Tour = require('./../models/tourModel');
 
-
-exports.getTour = (req, res) => {
+exports.getTour = async(req, res) => {
   console.log(req.params);
 
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id);
-
-  res.status(200).json({
-    status: 'success'
-
-    // data: {
-    //   tour
-    // }
-  });
-};
-
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
-};
-
-exports.createTour = async(req, res) => {
-
+  const id = req.params.id 
   try {
-    const newTour = await Tour.create(req.body)
+    const tour = await Tour.findById(id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'err',
+      message: 'invalid data set'
+    });
+  }
+};
+
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'err',
+      message: 'invalid data set'
+    });
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
     res.status(201).json({
       status: 'success',
       data: {
         tour: newTour
       }
     });
-  }
-  catch(err) {
+  } catch (err) {
     res.status(400).json({
-      status:'err',
-      message : 'invalid data set'
-    })
+      status: 'err',
+      message: 'invalid data set'
+    });
   }
 };
 
