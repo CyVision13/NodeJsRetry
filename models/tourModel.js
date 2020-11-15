5dac8c3476e2cae943ba60c9ec1f5b1546f0cce0
cyvision13 <cyvision13@outlook.com>
2020-11-15 08:24:27 -0500

    Modelling the Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,21 +1,53 @@
 const mongoose = require('mongoose');
 
-
 const tourSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, 'A tour must have a name'],
-      unique: true
-    },
-    rating: {
-      type: Number,
-      default: 4.5
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have a price']
-    }
-  });
-  const Tour = mongoose.model('Tour', tourSchema);
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group Size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String]
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
-  module.exports = Tour
+module.exports = Tour;
+ 
