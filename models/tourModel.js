ccb46530cb7a11fdce28ce3eda6647dfe38e15cf
cyvision13 <cyvision13@outlook.com>
2020-11-25 11:37:43 -0500

    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -53,7 +53,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour:{
+      type:Boolean,
+      default:false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -64,7 +68,7 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
-
+//**** */ Document middleware ***
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
@@ -78,6 +82,19 @@ tourSchema.pre('save', function(next) {
 //   console.log(doc);
 //   next();
 // });
+
+// *** Query Middleware
+
+tourSchema.pre(/^find/ ,function(next){
+  this.find({secretTour: {$ne:true}})
+
+  next();
+})
+
+tourSchema.post(/^find/ ,function(docs,next){
+  console.log(docs);
+  next();
+})
 
 const Tour = mongoose.model('Tour', tourSchema);
 
