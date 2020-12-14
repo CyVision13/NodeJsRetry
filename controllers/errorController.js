c12006d3c46063e051ef29d0fad41a698bbf142e
cyvision13 <cyvision13@outlook.com>
2020-12-14 09:26:32 -0500

    Errors During Development vs Production.mp4

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,43 @@
-module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-  
+const sendErrorDev = (err,res) =>{
+  res.status(err.statusCode).json({
+    status: err.status,
+    error:err,
+    message: err.message,
+    stack: err.stack
+  }); 
+}
+
+const sendErrorProd = (err,res) =>{
+  // Operational, trusted error: send message to client
+  if(err.isOperational){
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message
     });
+  } 
+  // programming or other unknown error: don't leak error details
+    else {
+      //1) log Error
+      console.error('Error ',err);
+
+      //2)Send generic message
+    res.status(500).json({
+      status:'error',
+      message: 'Something went very wrong'
+    })
+  }
+  
+}
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+  
+    if(process.env.NODE_ENV==='development'){
+      sendErrorDev(err,res)
+    }else if (process.env.NODE_ENV === 'production'){
+      sendErrorProd(err,res)
+    }
+
+    
   }
