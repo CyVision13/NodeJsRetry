872666b9befcdb4aa0bfe4bd51bfa5a7f4f6b40a
cyvision13 <cyvision13@outlook.com>
2020-12-14 09:47:01 -0500

    Handling Invalid Database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,43 +1,51 @@
-const sendErrorDev = (err,res) =>{
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    error:err,
+    error: err,
     message: err.message,
     stack: err.stack
-  }); 
-}
+  });
+};
 
-const sendErrorProd = (err,res) =>{
+const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
-  if(err.isOperational){
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message
     });
-  } 
-  // programming or other unknown error: don't leak error details
-    else {
-      //1) log Error
-      console.error('Error ',err);
-
-      //2)Send generic message
-    res.status(500).json({
-      status:'error',
-      message: 'Something went very wrong'
-    })
   }
-  
-}
+  // programming or other unknown error: don't leak error details
+  else {
+    //1) log Error
+    console.error('Error ', err);
+
+    //2)Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong'
+    });
+  }
+};
 
 module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-  
-    if(process.env.NODE_ENV==='development'){
-      sendErrorDev(err,res)
-    }else if (process.env.NODE_ENV === 'production'){
-      sendErrorProd(err,res)
-    }
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-    
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
+};
