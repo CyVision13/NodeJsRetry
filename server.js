b1cb9e76747a3297fd403c677a69ac06fbac793b
cyvision13 <cyvision13@outlook.com>
2020-12-17 04:46:52 -0500

    Errors Outside Express Unhandled Rejections

--- server.js
+++ server.js
@@ -5,20 +5,25 @@ const mongoose = require('mongoose');
 
 mongoose
   // .connect(process.env.DATABASE_LOCAL, {
-    .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    dbName: "natours",
-    useUnifiedTopology: true 
+    dbName: 'natours',
+    useUnifiedTopology: true
   })
   .then(() => console.log('DB connection successful!'));
 
-
-
-
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
