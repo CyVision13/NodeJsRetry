e27c127c3774d4cab319d1b8dec19c391c61a5ef
cyvision13 <cyvision13@outlook.com>
2020-12-17 04:48:18 -0500

    Errors Outside Express Unhandled Rejections Show All Err

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
+  
   console.log('Unhandled Rejection! Shutting down...');
+  console.log(err);
   server.close(() => {
     process.exit(1);
   });
