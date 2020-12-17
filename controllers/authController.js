da3c4679e4c525ddcea95ff6261dc80fd4f46383
cyvision13 <cyvision13@outlook.com>
2020-12-17 05:40:24 -0500

    Creating New Users Test On  heroku

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+
+exports.signup = catctAscync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+      status: 'success',
+      data:{
+          user: newUser
+      }
+  })
+});
