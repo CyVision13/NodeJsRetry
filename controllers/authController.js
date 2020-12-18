9e1fa6bc361e845967cd2efeb312b7ce46a5baca
cyvision13 <cyvision13@outlook.com>
2020-12-18 07:16:21 -0500

    Protecting Tour Routes - Part 2 section 0.0.2

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,7 +62,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
-    req.headers.authorization.startWith('Bearer')
+    req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
