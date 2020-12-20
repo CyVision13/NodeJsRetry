6b45e3687167f5ec8a43d32d5bf072299c9db524
cyvision13 <cyvision13@outlook.com>
2020-12-20 02:43:50 -0500

    Sending Emails with Nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const AppError = require('./../utils/appError');
-
+const sendEmail = require('./../utils/email');
 const signToken = id => {
   return jwt.sign(
     {
@@ -122,6 +122,33 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
   // 3) Send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfrim to :${resetURL}.\n If you didn't forgot your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token(valid for 10 min)',
+      message
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token send to email!'
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+
+    return next(new AppError('There was an Error sending the email. Try again later!',500))
+  }
+
   next();
 });
 exports.resetPassword = (req, res, next) => {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -15,7 +15,7 @@ router.post('/signup', signup);
 router.post('/login', login);
 
 router.post('/forgotPassword', forgotPassword);
-router.post('/resetPassword', resetPassword);
+router.patch('/resetPassword/:token', resetPassword);
 
 router
   .route('/')
